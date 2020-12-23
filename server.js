bf3ec5da588edec542be38fe10060513ceecda38
davidgrunblatt <david@davids-MBP-2.lan>
2020-12-22 17:26:51 -0800

    tweaked

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const bodyP = require('body-parser');
-// import routes
-// require('./routes/exporter')(app);
 
 // middleware
 app.use(bodyP.urlencoded());
